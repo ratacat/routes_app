15cc5dc50d7b73d5fd03c138b07b0599d29e4f6d
ratacat <neuralsplash@gmail.com>
2015-06-04 14:31:13 -0700

    working create

---
+++ app/controllers/planes_controller.rb
@@ -0,0 +1,16 @@
+class PlanesController < ApplicationController
+	def index
+		@planes = Plane.all
+		render :index
+	end
+	def new
+		render :new
+	end
+	def create
+		plane = params.require(:plane).permit(:name, :design, :description)
+		Plane.create(plane)
+		redirect_to "/planes"
+	end
+end
+
+
